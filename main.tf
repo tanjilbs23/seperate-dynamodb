6b1ca4753a3470aae51fa8cea5cab55a4226f898
tanjilbs23 <tanjil.bs23@gmail.com>
2022-09-23 14:55:45 +0600

    variables

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 module "trip-dynamodb" {
   source = "./dynamodb/trip"
-#   environment = "dev"
 }
 module "organization-dynamodb" {
   source = "./dynamodb/organization"
-#   environment = "dev"
 }
