7757f78a83609ea1e741adeb95074dece1ece778
tanjilbs23 <tanjil.bs23@gmail.com>
2022-10-11 02:18:17 +0600

    with datasource

--- dynamodb/organization/organization_table.tf
+++ dynamodb/organization/organization_table.tf
@@ -17,3 +17,60 @@ module "dynamodb-table" {
     Environment = var.environment
   }
 }
+
+# Iam Role
+
+
+resource "aws_iam_role" "example" {
+  name = "example"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "appsync.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "example" {
+  name = "example"
+  role = aws_iam_role.example.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "dynamodb:*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_dynamodb_table.example.arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+# 
+
+resource "aws_appsync_datasource" "example" {
+  api_id           = "a53izjzw3bhidmnnaoqqqjwn54"
+  name             = "test"
+  service_role_arn = aws_iam_role.example.arn
+  type             = "AMAZON_DYNAMODB"
+
+  dynamodb_config {
+    table_name = module.dynamodb-table.dynamodb_table_id
+  }
+}
